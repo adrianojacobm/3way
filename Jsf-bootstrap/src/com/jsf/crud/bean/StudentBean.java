2387590d31f843ac0f5e41e2c203066f95b8742d
VictorMenezes0 <maisumdiaieshua@users.noreply.github.com>
2018-07-22 12:37:33 -0300

    projeto jsf com bootstrap

---
+++ Jsf-bootstrap/src/com/jsf/crud/bean/StudentBean.java
@@ -0,0 +1,106 @@
+package com.jsf.crud.bean;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import com.jsf.crud.db.operations.DatabaseOperation;
+import com.jsf.crud.entities.*;
+
+@ManagedBean 
+@RequestScoped
+public class StudentBean {
+
+//	private int id;  
+//	private String name;  
+//	private String email;  
+//	private String password;  
+//	private String gender;  
+//	private String address;
+
+	public ArrayList<Student>studentsListFromDB;
+	private Student Student = new Student();
+
+//	public int getId() {
+//		return id;	
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getGender() {
+//		return gender;
+//	}
+//
+//	public void setGender(String gender) {
+//		this.gender = gender;
+//	}
+//
+//	public String getAddress() {
+//		return address;
+//	}
+//
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}  
+	
+	@PostConstruct
+	public void init() {
+		studentsListFromDB = DatabaseOperation.getStudentsListFromDB();
+	}
+
+	public ArrayList<Student> studentsList() {
+		return studentsListFromDB;
+	}
+	
+	public String saveStudentDetails(Student newStudentObj) {
+		return DatabaseOperation.saveStudentDetailsInDB(newStudentObj);
+	}
+	
+	public String editStudentRecord(int studentId) {
+		return DatabaseOperation.editStudentRecordInDB(studentId);
+	}
+	
+	public String updateStudentDetails(Student updateStudentObj) {
+		return DatabaseOperation.updateStudentDetailsInDB(updateStudentObj);
+	}
+	
+	public String deleteStudentRecord(int studentId) {
+		return DatabaseOperation.deleteStudentRecordInDB(studentId);
+	}
+
+	public Student getStudent() {
+		return Student;
+	}
+
+	public void setStudent(Student student) {
+		Student = student;
+	}
+}
